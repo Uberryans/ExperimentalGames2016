1aa5ada12b050e77db4e40c660f3434626c2ff5d
Kyshi <coltonthefirst@gmail.com>
2016-02-09 19:42:44 -0500

    adding apathetic cyborg invaders

---
+++ apathetic cyborg invaders/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * 2D Character Controller for Player. Will likely eventually become a subclass of a Controller class that will also include enemies with similar behavior.
+ */
+
+public class EnemyController : MonoBehaviour
+{
+
+    //public float maxSpeed = 3; //Max speed value allowed
+
+    Transform target;
+    Transform enemyTransform;
+    public float speed = 3f;
+    public float rotationSpeed = 3f;
+
+    private float move;
+
+    bool right = true; //What Direction Is Player Facing
+
+
+    bool attack = false;
+
+
+    void Start()
+    {
+        enemyTransform = this.GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+        Vector2 targetHeading = target.position - transform.position;
+        Vector2 targetDirection = targetHeading.normalized;
+
+        move = target.position.x - transform.position.x;
+
+        //rotate to look at the player
+
+        if (move > 0 && !right)
+        {
+            Flip();
+        }
+        else if (move < 0 && right)
+        {
+            Flip();
+        }
+
+
+        //move towards the player
+        GetComponent<Rigidbody2D>().velocity = new Vector2(speed, GetComponent<Rigidbody2D>().velocity.y); //move object
+        //enemyTransform.position += enemyTransform.forward * speed * Time.deltaTime;
+
+
+    }
+
+
+    /*
+ * Flip switches the facing direction of the character. 
+ */
+    void Flip()
+    {
+        right = !right;
+        Vector3 scale = transform.localScale;
+        scale.x = scale.x * -1;
+        transform.localScale = scale;
+        speed = speed * -1;
+    }
+
+}
---
+++ apathetic cyborg invaders/Assets/Scripts/tempdeath.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tempdeath : MonoBehaviour {
+
+    public float health = 100;
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            health += -10;
+        }
+            
+    }
+        // Use this for initialization
+        void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
