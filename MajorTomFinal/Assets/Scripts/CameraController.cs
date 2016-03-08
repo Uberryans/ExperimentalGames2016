a6048ce76ce4d612250a53d407fd95269e578290
Kyshi <coltonthefirst@gmail.com>
2016-03-07 23:12:33 -0500

    yay sounds

---
+++ MajorTomFinal/Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    void Update()
+    {
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        if (yaw < -60)
+        { yaw = -60; }
+
+        if (yaw > 60)
+        { yaw = 60; }
+
+        if (pitch > 20)
+        { pitch = 20; }
+
+        if (pitch < -60)
+        { pitch = -60; }
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+}
+
---
+++ MajorTomFinal/Assets/Scripts/DBTunes.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DBTunes : MonoBehaviour
+{
+
+    public AudioSource AS;
+    public AudioClip[] list;
+    public int i = 0;
+
+
+    void Start()
+    {
+        AS = gameObject.GetComponent<AudioSource>();
+
+        list = new AudioClip[]{(AudioClip)Resources.Load("david bowie - changes"),
+                                     (AudioClip)Resources.Load("David Bowie - Fame"),
+                                     (AudioClip)Resources.Load("David Bowie - Heroes   lyrics"),
+                                     (AudioClip)Resources.Load("David Bowie - Rebel Rebel"),
+                                     (AudioClip)Resources.Load("David Bowie - Suffragette city"),
+                                     (AudioClip)Resources.Load("David Bowie - Ziggy Stardust"),
+                                     (AudioClip)Resources.Load("David Bowie- Space Oddity Original Video (1969)"),
+                                     (AudioClip)Resources.Load("David Bowie Young Americans"),
+                                     (AudioClip)Resources.Load("DAVID BOWIE_____THE MAN WHO SOLD THE WORLD")};
+    }
+
+
+
+    void Update()
+    {
+        AS.volume += Input.GetAxis("Mouse ScrollWheel");
+
+        if (!AS.isPlaying)
+        {
+            if (i == 8)
+            {
+                i = 0;
+            }
+            else { i++; }
+            AS.clip = list[i];
+            AS.Play();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (i == 0)
+            {
+                i = 8;
+            }
+            else { i--; }
+            AS.clip = list[i];
+            AS.Play();
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (i == 8)
+            {
+                i = 0;
+            }
+            else { i++; }
+            AS.clip = list[i];
+            AS.Play();
+        }
+    }
+
+}
